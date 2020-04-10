7de463f735534c346a55f8d070b0536a832ffe5b
José Reyes <jrpalacio.dev@gmail.com>
2020-04-09 21:49:16 -0500

    Route single page app

---
+++ routes.js
@@ -0,0 +1,14 @@
+const router = [
+    {
+        path: '/',
+        template: '<h1> Home </h1>'
+    },
+    {
+        path: '/contact',
+        template: '<h1> Contacto </h1>'
+    },
+    {
+        path: '/about',
+        template: '<h1> Acerca de  </h1>'
+    },
+]
